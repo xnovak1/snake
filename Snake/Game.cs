c10603129d08aa3e6775a5c1ae3453592848f0dc
Michal Novák <xnovak9@fi.muni.cz>
2023-09-14 11:51:09 +0200

    Classes init

---
+++ Snake/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Game
+    {
+        private const int Width = 10;
+        private const int Height = 10;
+    }
+}
